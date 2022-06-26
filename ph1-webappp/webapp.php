622e634ee16f3b852d99fb1a3d76446e39667415
Your Name <you@example.com>
2022-06-26 11:34:24 +0900

    仮

---
+++ ph1-webappp/webapp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>HTML内でのPHP処理</title>
+</head>
+<body>
+<h1><?php echo 'タイトルです'; ?></h1>
+<?php
+$text = "HTML内でのPHPスクリプト実行処理";
+echo "<p> $text </p>";
+?>
+</body>
+</html>
