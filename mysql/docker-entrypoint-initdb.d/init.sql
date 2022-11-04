aed992338a549da6f2883e3c8aba11ec301e34b4
mt-tannen <mnr.east6935@gmail.com>
2022-11-04 18:07:13 +0900

    テーブル作成した

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA quizy;
 USE quizy;
 
 CREATE TABLE big_questions (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEYdoc,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
 
@@ -14,3 +14,39 @@ INSERT INTO
 VALUES
   ("東京の難読地名クイズ"),
   ("広島県の難読地名クイズ");
+
+DROP TABLE IF EXISTS questions;
+CREATE TABLE questions (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  big_question_id INT NOT NULL,
+  image VARCHAR(255)
+);
+
+INSERT INTO questions
+(big_question_id, image)
+VALUES
+  (1, "takanawa.jpg"),
+  (1, "kameido.jpg"),
+  (2, "mukainada.jpg");
+
+DROP TABLE IF EXISTS choices;
+CREATE TABLE choices (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  quistion_id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  valid BOOLEAN NOT NULL
+);
+
+INSERT INTO choices
+(quistion_id, name, valid)
+VALUES
+  (1, "たかなわ", 1),
+  (1, "たかわ", 0),
+  (1, "こうわ", 0),
+  (2, "かめと", 0),
+  (2, "かめど", 0),
+  (2, "かめいど", 1),
+  (3, "むこうひら", 0),
+  (3, "むかいこうじ", 0),
+  (3, "むかいなだ", 1);
+
--- src/html/index.php
+++ src/html/index.php
@@ -1,5 +1,10 @@
 <?php
+require('dbconnect.php');
+
+$stmt = $db->query('SELECT * FROM big_questions');
+$big_questions = $stmt->fetchAll();
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -14,6 +19,11 @@
 
 <body>
     <div>
+    <?php foreach ($big_questions as $big_question) : ?>
+    <p>
+      <a href="/quiz?question_id=<?php echo $big_question['id']; ?>"><?php echo $big_question['id'] . '：' . $big_question['name']; ?></a>
+    </p>
+  <?php endforeach; ?>
         <script src="./quizy.js"></script>
     </div>
 </body>
